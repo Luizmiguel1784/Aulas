0a5a299b5b69c1380720a95c5173d4fc305d882b
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2025-02-04 10:03:30 -0300

    Add files via upload

---
+++ Olamundo.java
@@ -0,0 +1,12 @@
+package aula28janeiro;
+public class Olamundo {
+	public static void main(String[] args) {
+		//isso é um comentario 	
+		//saida
+		//System.out.println("Olá turma 1DM!"); //instrução de saida
+		System.out.println("primeira linha");
+		System.out.println(" Segunda linha");
+		
+		System.out.print(" Esse texto");
+		
+	    System.out.print(" Fica junto do outro");} }
